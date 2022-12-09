62fe18212d10b50de8b1981b4b72ca71feec635e
Tobs <tobaodumosu1@gmail.com>
2022-12-09 16:42:58 +0000

    Major: Added interactivity to cards

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const card1 = document.querySelector('.card:nth-child(7)');
+const card2 = document.querySelector('.card:nth-child(6)');
 
-const test = () => {
-    card1.classList.toggle('is-active')
+const fadeIn = () => {
+    card1.classList.toggle('fadein')
+    card2.classList.toggle('pushed')
 }
 
-card1.addEventListener('click', test);
+card1.addEventListener('click', fadeIn);
+
+/* if card has property of fadein
+add event listener click fadout
+else do nothing */
+
+const fadeOut = () => {
+    console.log("Fades Out")
+}

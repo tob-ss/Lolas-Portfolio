96104b05ee37ddad3d5e5e34dd38e746a47e1ef1
Tobs <tobaodumosu1@gmail.com>
2022-12-10 17:47:03 +0000

    Chore, tweaking interactive functionality to first two cards

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+
+/* when click on card it goes into zoomed state
+
+when in zoomed state, */
+
+let zoomed1 = false
+
 const card1 = document.querySelector('.card:nth-child(7)');
+
 const card2 = document.querySelector('.card:nth-child(6)');
 
-const fadeIn = () => {
-    card1.classList.toggle('fadein')
-    card2.classList.toggle('pushed')
+const notCards = document.querySelectorAll(':not(.card)');
+
+const zoom1 = () => {
+   zoomed1 = true 
+    card1.classList.add('zoom')
+    card2.classList.add('pushed')
+    console.log(zoomed1)
 }
 
-card1.addEventListener('click', fadeIn);
+if (zoomed1 === false) {
+    card1.addEventListener('click', zoom1)
+} else {
+    notCards.forEach(element => {
+        notCards.addEventListener('click', () => {
+            zoomed1 = false
+                card1.classList.remove('zoom')
+                card2.classList.remove('pushed')
+        });
+    })}
+/* card click on should zoom up
 
-/* if card has property of fadein
-add event listener click fadout
-else do nothing */
+card behind clicked card should be pushed back
 
-const fadeOut = () => {
-    console.log("Fades Out")
-}
+clicking on clicked card shouldn't put it back
+
+clicking anywhere apart from the card stack should put the card back
+
+clicking on a different card should put the zoomed card back, return the pushed card back and zoom up the new card */
